6d5602d848a1ec0b90ea31bc90f0dac3c83cf8bd
Timofey Kozich <kozich.timofey@gmail.com>
2024-02-22 14:26:15 +0300

    feat: solve task compareObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -88,7 +88,15 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
 function compareObjects(obj1, obj2) {
-  return [obj1, obj2];
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+  for (let i = 0; i < keys1.length; i += 1) {
+    if (!keys2.includes(keys1[i])) return false;
+    if (obj1[keys1[i]] !== obj2[keys1[i]]) return false;
+  }
+  return true;
 }
 
 /**
